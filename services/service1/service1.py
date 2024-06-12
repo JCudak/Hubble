32d5e5144207be735fd7f4c369ec49753c2bf919
Piotr Aksamit <pa4166.m@gmail.com>
2024-06-13 00:03:01 +0200

    feat: add data to db

--- services/service1/service1.py
+++ services/service1/service1.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
-from bson import ObjectId
+import random
 
 app = Flask(__name__)
 
@@ -9,11 +9,13 @@ client = MongoClient("mongodb://suu:suu@mongo-mongodb-headless:27017/suu_db")
 db = client.suu_db
 collection = db.mycollection
 
+
 def serialize_document(document):
     """ Helper function to convert MongoDB document to JSON serializable format """
     if '_id' in document:
         document['_id'] = str(document['_id'])
     return document
+
 
 @app.route('/data', methods=['GET'])
 def get_data():
@@ -22,5 +24,27 @@ def get_data():
         data = serialize_document(data)  # Convert ObjectId to string
     return jsonify(data), 200
 
+
+def insert_initial_data():
+    """ Function to insert initial data into MongoDB if collection is empty """
+    count = collection.count_documents({})
+    if count == 1000:
+        return
+
+    data_to_insert = []
+    for i in range(1000 - count):
+        data = {
+            'name': f'User {i + 1}',
+            'age': random.randint(20, 60),
+            'city': 'City Name'
+            # Add more fields as needed
+        }
+        data_to_insert.append(data)
+
+    result = collection.insert_many(data_to_insert)
+    print(f"Inserted {len(result.inserted_ids)} documents into 'mycollection'")
+
+
 if __name__ == '__main__':
+    insert_initial_data()
     app.run(host='0.0.0.0', port=5001)
